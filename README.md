00d1c06d012959df7e1ab10131040bdd654572da
MrPowerUp82 <gustavogostozo8@gmail.com>
2022-01-30 05:23:34 -0300

    push

---
+++ README.md
@@ -0,0 +1,19 @@
+# TubeDownloader
+
+## Instalação das Libs
+
+python -m pip install -r requirements.txt
+
+### `Crirar ".exe"`
+
+1. python -m pip install pyinstaller
+2. python -m eel main.py web --noconsole --onefile -i web/icon.ico --runtime-tmpdir %temp%/pysound
+
+### `Executar`
+
+python main.py
+
+## Exemplo
+
+![alt text](screenshot.png)
+![alt text](screenshot2.png)
---
+++ main.py
@@ -0,0 +1,38 @@
+import eel
+import os
+from moviepy.editor import VideoFileClip
+from pytube import YouTube
+
+eel.init('web')
+
+@eel.expose
+def get_info(url):
+    yt=YouTube(url)
+    return [yt.title,yt.thumbnail_url]
+
+@eel.expose
+def download(url,option):
+    yt=YouTube(url)
+    if option == 'mp3':
+        name = yt.title.replace('(','').replace(')','')
+        before_exist = [True for x in os.listdir('.') if name in x]
+        if before_exist:
+            file_name_ = [x for x in os.listdir('.') if name in x][0]
+            os.rename(file_name_,file_name_.replace('.mp4','.await'))
+        yt.streams.filter(file_extension='mp4').first().download()
+        file_name = [x for x in os.listdir('.') if name in x and '.mp4' in x][0]
+        video = VideoFileClip(file_name)
+        video.audio.write_audiofile(file_name.replace('.mp4','.mp3'))
+        video.close()
+        if before_exist:
+            os.rename(file_name_,file_name_.replace('.await','.mp4'))
+            return
+        else:
+            os.remove(file_name)
+    if option == '360p':
+        yt.streams.filter(res="360p",file_extension='mp4').first().download()
+    if option == '720p':
+        yt.streams.filter(res="720p",file_extension='mp4').first().download()
+
+
+eel.start('index.html', size=(400,150))
